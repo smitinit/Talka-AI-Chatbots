4ad8c367bd41f5af841986316723a60703ba3013
smitinit <patelsmit090305@gmail.com>
2025-06-21 12:30:08 +0530

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,16 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher(["/bots(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect();
+  if (isProtectedRoute(req)) {
+    await auth.protect();
+  }
 });
+
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
+    // Skip static assets and internal Next.js routes
+    "/((?!_next|.*\\.(?:js|css|png|jpg|jpeg|svg|ico|woff2?|ttf|eot|map)).*)",
+    // Always apply to API and tRPC
     "/(api|trpc)(.*)",
   ],
 };
